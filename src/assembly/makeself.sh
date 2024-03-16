b5dedd95bcf02be54d5cebb0637de6b8d49480e4
ahartman30 <ahartman30@users.noreply.github.com>
2024-03-16 17:04:18 +0100

    Added makeself installer.

--- src/assembly/makeself.sh
+++ src/assembly/makeself.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Script is executed after installation
+version=$1
+chmod a+x $PWD/mosmix-kml-tool.sh
+mv $PWD/mosmix-kml-tool.sh $PWD/mosmix-kml-tool-${version}.sh
 
 # Remove script itself after installation
 rm $PWD/makeself.sh
--- src/assembly/mosmix-kml-tool.sh
+++ src/assembly/mosmix-kml-tool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 root=$(dirname $0)
-$root/jdk/bin/java -jar $root/mosmix-kml-tool-1.1.0.jar
+$root/jdk/bin/java -jar $root/mosmix-kml-tool-${project.version}.jar
 
